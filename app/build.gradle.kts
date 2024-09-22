28c8d8c50cbccddeadaccc2afcc563a521e9433f
Chieler Li <chielerli@gmail.com>
2024-09-22 18:32:45 -0500

    Added .tfLite file but need to add it to asset folder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
@@ -44,4 +43,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.tensorflowLite)
+    implementation(libs.tensorflowLiteSupport)
+    implementation(libs.tensorflowLiteGpu)
 }
